c362f3c07b39bfccc045dd94e12f0e7722eb37c1
salmaouirini <ouirinisalma1@gmail.com>
2024-10-06 17:49:20 -0400

    Implement API endpoints using Express-Mongo

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find({ state: req.params.state });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    const dealerId = parseInt(req.params.id, 10); 
+    console.log(`Fetching dealer with ID: ${dealerId}`);
+    try {
+        const document = await Dealerships.findOne({ id: dealerId }); 
+        if (!document) {
+            return res.status(404).json({ error: 'Dealer not found' });
+        }
+        res.json(document);
+    } catch (error) {
+        console.error("Error fetching dealer:", error);
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 //Express route to insert review
